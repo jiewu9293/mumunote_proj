b0fe3f6b99f0c10c96aa455ce22f23850f8784af
jiewu deng <jiewu9293@gmail.com>
2026-08-16 22:42:50 +0800

    feat: add article image path config and format article data for frontend

--- app/config/config.py
+++ app/config/config.py
@@ -2,6 +2,7 @@
 class Config(object):
     db_url = "mysql+pymysql://admin1:123@192.168.1.129:3306/mumushouji"
     page_count = 10
+    article_header_image_path = "/images/article/header/"   
 
 # 测试环境
 class TestConfig(Config):
--- controller/index.py
+++ controller/index.py
@@ -2,11 +2,21 @@ import logging
 
 from flask import Blueprint, render_template,request
 
+from app.config.config import config
+from app.settings import env
 from model.article import Article
 
 index = Blueprint("index",__name__)
 
-
+label_types = {
+    "recommend":{"name":"推荐","selected":"selected"},
+    "auto_test":{"name":"自动化测试","selected":"no-selected"},
+    "python":{"name":"Python","selected":"no-selected"},
+    "java":{"name":"Java","selected":"no-selected"},
+    "function_test":{"name":"功能测试","selected":"no-selected"},
+    "perf_test":{"name":"性能测试","selected":"no-selected"},
+    "funny":{"name":"幽默段子","selected":"no-selected"},
+}
 @index.route("/")
 def home():
     page = request.args.get("page")
@@ -21,7 +31,12 @@ def home():
     #     到数据库中查询文章数据，然后返回给前端页面
     article = Article()
     db_result = article.find_article(page, article_type)
+    for article, nickname in db_result:
+        article.label = label_types.get(article.label_name).get("name")
 
+        article.create_time = str(article.create_time.month) + '.' + str(article.create_time.day)
+
+        article.article_image = config[env].article_header_image_path + str(article.article_image)
 
     return render_template("index.html",result=db_result)
 
