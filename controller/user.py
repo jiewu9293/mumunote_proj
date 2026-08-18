a232c43a0cb3295160b41365b3ec9b6c00653451
jiewu deng <jiewu9293@gmail.com>
2026-08-18 22:15:21 +0800

    feat: implement user registration

--- controller/user.py
+++ controller/user.py
@@ -1,10 +1,11 @@
 import re
-
+import json
 from flask import Blueprint,request,make_response,session
-
+import hashlib
 from common import response_message
 from common.email_utils import get_email_code, send_email
 from common.utils import ImageCode
+from model.user import User
 
 user = Blueprint("user",__name__)
 
@@ -20,7 +21,7 @@ def vcode():
 
 @user.route("/ecode",methods=["POST"])
 def email_code():
-    email = request.form.get("email")
+    email = json.loads(request.data).get("email")
 
     if not re.match(".+@.+\..+", email):
         return response_message.UserMessage.other("无效的邮箱")
@@ -36,3 +37,33 @@ def email_code():
         print(e)
         return response_message.UserMessage.error("邮件发送失败")
     return code
+
+@user.route("/reg",methods=["post"])
+def register():
+    #把前端发来的 JSON 转成 Python 字典
+    request_data = json.loads(request.data)
+    username=request_data.get("username")
+    password=request_data.get("password")
+    second_password=request_data.get("second_password")
+    ecode=request_data.get("ecode")
+    # 做数据的验证
+    if ecode.lower() != session.get("ecode"):
+        return response_message.UserMessage.error("邮箱验证码错误")
+    # 用户名 和 密码的验证
+    if not re.match(".+@.+\..+", username):
+        return response_message.UserMessage.other("无效的邮箱")
+
+    if len(password) < 6:
+        return response_message.UserMessage.error("密码不合法")
+
+    if password != second_password:
+        return response_message.UserMessage.error("两次密码不一致")
+    # 用户名是否已经注册
+    user = User()
+    if len(user.find_by_username(username=username)) > 0:
+        return response_message.UserMessage.error("用户名已经存在")
+
+        # 实现注册的功能了
+    password = hashlib.md5(password.encode()).hexdigest()
+    result = user.do_register(username=username, password=password)
+    return response_message.UserMessage.success("用户注册成功")
--- model/user.py
+++ model/user.py
@@ -1,3 +1,5 @@
+import random
+
 from sqlalchemy import Table
 
 from common.database import db_connect
@@ -8,3 +10,21 @@ db_session,Base,engine = db_connect()
 class User(Base):
     # 表结构的反射加载
     __table__ = Table("user",Base.metadata,autoload_with=engine)
+
+    def find_by_username(self, username):
+        return db_session.query(User).filter_by(username=username).all()
+
+    def do_register(self, username, password):
+        nickname = username.split("@")[0]
+        # 头像
+        picture_num = random.randint(1, 539)
+        picture = str(picture_num) + ".jpg"
+        job = "未定义"
+        user = User(username=username,
+                    password=password,
+                    nickname=nickname,
+                    picture=picture,
+                    job=job)
+        db_session.add(user)
+        db_session.commit()
+        return user
