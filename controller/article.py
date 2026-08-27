09dd73f4ce7612e0017a5c13535252fb56e629d1
jiewu deng <jiewu9293@gmail.com>
2026-08-27 21:07:24 +0800

    feat(comment): add comment information display

--- controller/article.py
+++ controller/article.py
@@ -4,7 +4,9 @@ from flask import Blueprint, render_template,request
 
 from app.config.config import config
 from app.settings import env
+from model import feedback
 from model.article import Article
+from model.feedback import Feedback
 from model.user import User
 
 article = Blueprint("article",__name__)
@@ -24,7 +26,14 @@ def article_detail():
     # 相关文章的功能
     about_article = article.find_about_article(article_content.label_name)
 
+    #@todo  待办查看评论信息
+    feedback_data_list  = Feedback().get_feedback_user_list(article_id)
+
+    feedback_count = Feedback().get_article_feedback_count(article_id)
+
     is_favorite = 1
     return render_template("article_info.html",article_content=article_content,
                            user_info=user_info,is_favorite=is_favorite,
-                           article_tag_list=article_tag_list,about_article=about_article)
+                           article_tag_list=article_tag_list,about_article=about_article,
+                           feedback_data_list=feedback_data_list,
+                           feedback_count = feedback_count)
---
+++ model/feedback.py
@@ -0,0 +1,94 @@
+import random
+
+from sqlalchemy import Table
+
+from common.database import db_connect
+from config.config import config
+from model.user import User
+from settings import env
+from common.utils import model_to_json
+
+db_session,Base,engine = db_connect()
+
+
+class Feedback(Base):
+    # 表结构的反射加载
+    __table__ = Table("comment",Base.metadata,autoload_with=engine)
+
+    def get_feedback_user_list(self,article_id):
+        final_data_list = []
+        #查询一级评论
+        feedback_list = self.find_feedback_by_article_id(article_id)
+        for feedback in feedback_list:
+            user = User()
+            # 根据一级评论的数据，获取回复评论的评论的内容
+            all_reply = self.find_reply_by_replyid(base_reply_id=feedback.id)
+            #在找：这条一级评论是谁发的。
+            feedback_user = user.find_by_userid(feedback.user_id)
+            #遍历一级评论的每个评论
+            reply_list = []
+            # 再根据每一条回复的评论，查询用户信息
+            for reply in all_reply:
+                # 用于存储当前这条原始评论的所有回复评论，如果没有回复，这个值就为空
+                reply_content_with_user = {}
+                #获取回复评论的用户信息
+                from_user_data = user.find_by_userid(reply.user_id)
+                #获取被回复评论的用户信息
+                to_user_reply_data = self.find_reply_by_id(reply.reply_id)
+                to_user_data = user.find_by_userid(to_user_reply_data[0].user_id)
+
+                reply_content_with_user["from_user"] = model_to_json(from_user_data)
+                reply_content_with_user["to_user"] = model_to_json(to_user_data)
+                reply_content_with_user["content"] = model_to_json(reply)
+                reply_list.append(reply_content_with_user)
+
+            every_feedback_data = model_to_json(feedback)
+            every_feedback_data.update(model_to_json(feedback_user))
+            every_feedback_data["reply_list"] = reply_list
+            final_data_list.append(every_feedback_data)
+        return final_data_list
+
+
+
+
+    def find_feedback_by_article_id(self,article_id):
+       result = db_session.query(Feedback).filter_by(
+            article_id=article_id,
+           #一级评论的两个条件
+            base_reply_id=0,
+            reply_id=0,
+        ).order_by(
+            Feedback.id.desc()
+        ).all()
+
+       return result
+
+    def find_reply_by_id(self, id):
+        result = db_session.query(Feedback).filter(
+            Feedback.id == id
+        ).order_by(
+            Feedback.id.desc()
+        ).all()
+        return result
+
+    def find_reply_by_replyid(self, base_reply_id):
+        #根据某条一级评论的 id，找到它下面所有的回复评论，并按照回复时间（id）倒序排列
+        result = db_session.query(Feedback).filter_by(
+            base_reply_id=base_reply_id
+        ).order_by(
+            Feedback.id.desc()
+        ).all()
+        return result
+
+    def get_article_feedback_count(self, article_id):
+        result = db_session.query(Feedback).filter_by(
+            article_id=article_id,
+            reply_id=0,
+            base_reply_id=0
+        ).count()
+        return result
+
+
+
+
+
